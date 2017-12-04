f9ad5c89b59cf8880d39ff2650739c273e45d67f
Max Pridgen <jmpridgen@gmail.com>
2017-12-04 14:09:17 -0500

    Changes to spacemacs dotfile

--- spacemacs-dotfiles/init.el
+++ spacemacs-dotfiles/init.el
@@ -4,8 +4,8 @@
 
 (defun dotspacemacs/layers ()
   "01/14/2018Configuration Layers declaration.
-You should not put any user code in this function besides modifying the variable
-values."
+   You should not put any user code in this function besides modifying the variable
+   values."
   (setq-default
    ;; Base distribution to use. This is a layer contained in the directory
    ;; `+distribution'. For now available distributions are `spacemacs-base'
@@ -29,46 +29,50 @@ values."
    ;; Paths must have a trailing slash (i.e. `~/.mycontribs/')
    dotspacemacs-configuration-layer-path '()
    ;; List of configuration layers to load.
-   dotspacemacs-configuration-layers
-   '(
-     python
-     markdown
-     javascript
-     react
-     ruby
-     yaml
-     evil-commentary
-     ;; auto-completion
-     emacs-lisp
-     git
-     (shell :variables
-            shell-default-height 30
-            shell-default-position 'bottom)
-     syntax-checking
-     (version-control :variables
-                      version-control-diff-side 'left
-                      version-control-diff-tool 'diff-hl
-     )
-     ranger
-     osx
-     themes-megapack
-     ;; vim-powerline
-     ;; spell-checking
-     ;; markdown
-     ;; org
-     )
+   dotspacemacs-configuration-layers '(
+                                       python
+                                       markdown
+                                       (javascript :variables
+                                                   js2-mode-show-parse-errors nil
+                                                   js2-mode-show-strict-warnings nil
+                                                   )
+                                       react
+                                       ruby
+                                       yaml
+                                       evil-commentary
+                                       ;; auto-completion
+                                       emacs-lisp
+                                       git
+                                       (shell :variables
+                                               shell-default-height 30
+                                               shell-default-position 'bottom)
+                                       syntax-checking
+                                       (version-control :variables
+                                                         version-control-diff-side 'left
+                                                         version-control-diff-tool 'diff-hl
+                                       )
+                                       ranger
+                                       osx
+                                       themes-megapack
+                                       ;; vim-powerline
+                                       ;; spell-checking
+                                       ;; markdown
+                                       ;; org
+                                       )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(groovy-mode emacs-inertial-scroll)
+   dotspacemacs-additional-packages '(
+                                      groovy-mode
+                                      all-the-icons
+                                      )
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
-   dotspacemacs-excluded-packages
-    '(
-      smartparens
-     )
+   dotspacemacs-excluded-packages '(
+                                    smartparens
+                                    )
    ;; Defines the behaviour of Spacemacs when installing packages.
    ;; Possible values are `used-only', `used-but-keep-unused' and `all'.
    ;; `used-only' installs only explicitly used packages and uninstall any
@@ -80,10 +84,10 @@ values."
 
 (defun dotspacemacs/init ()
   "Initialization function.
-This function is called at the very startup of Spacemacs initialization
-before layers configuration.
-You should not put any user code in there besides modifying the variable
-values."
+   This function is called at the very startup of Spacemacs initialization
+   before layers configuration.
+   You should not put any user code in there besides modifying the variable
+   values."
   ;; This setq-default sexp is an exhaustive list of all the supported
   ;; spacemacs settings.
   (setq-default
@@ -121,6 +125,7 @@ values."
    ;; by your Emacs build.
    ;; If the value is nil then no banner is displayed. (default 'official)
    dotspacemacs-startup-banner 'official
+   ;;
    ;; List of items to show in startup buffer or an association list of
    ;; the form `(list-type . list-size)`. If nil then it is disabled.
    ;; Possible values for list-type are:
@@ -136,8 +141,8 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(solarized-dark
-                         spacemacs-light)
+   ;; dotspacemacs-themes '(solarized-dark
+   ;;                       spacemacs-light)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
@@ -306,18 +311,21 @@ values."
    ;; mouse-wheel-progressive-speed nil ;; don't accelerate scrolling
    ;; mouse-wheel-follow-mouse 't ;; scroll window under mouse
    ;; scroll-step 1
-   dotspacemacs-frame-title-format "%t"
-   ))
+   dotspacemacs-frame-title-format "%t - %b"
+   )
+  )
 
 
 (defun dotspacemacs/user-init ()
   "Initialization function for user code.
-It is called immediately after `dotspacemacs/init', before layer configuration
-executes.
- This function is mostly useful for variables that need to be set
-before packages are loaded. If you are unsure, you should try in setting them in
-`dotspacemacs/user-config' first."
-  )
+   It is called immediately after `dotspacemacs/init', before layer configuration
+   executes.
+    This function is mostly useful for variables that need to be set
+   before packages are loaded. If you are unsure, you should try in setting them in
+   `dotspacemacs/user-config' first."
+   (push '("melpa-stable" . "stable.melpa.org/packages/") configuration-layer-elpa-archives)
+   (push '(use-package . "melpa-stable") package-pinned-packages)
+)
 
 (defun my-setup-indent (n)
   ;; java/c/c++
@@ -366,7 +374,14 @@ before packages are loaded. If you are unsure, you should try in setting them in
   ;; This makes sure that diff-hl dired mode is enabled when dired opened
   (diff-hl-dired-mode t)
   (add-hook 'dired-mode-hook 'diff-hl-dired-mode)
-  )
+
+  ;; set the powerline separators
+  ;; (setq ns-use-srgb-colorspace nil)
+  (setq powerline-default-separator 'utf-8)
+
+  ;; stop javascript warnings from js2 in javascript layer
+  (js2-mode-hide-warnings-and-errors)
+)
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
@@ -392,3 +407,35 @@ before packages are loaded. If you are unsure, you should try in setting them in
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(defun dotspacemacs/emacs-custom-settings ()
+  "Emacs custom settings.
+This is an auto-generated function, do not modify its content directly, use
+Emacs customize menu instead.
+This function is called at the very end of Spacemacs initialization."
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("71b9b4c5d2a5126586d204e20c3fb4797f70d3d057a0c8b03bac2c51893007a2" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "2a739405edf418b8581dcd176aaf695d319f99e3488224a3c495cb0f9fd814e3" "3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" default)))
+ '(evil-want-Y-yank-to-eol nil)
+ '(package-selected-packages
+   (quote
+    (spaceline-all-the-icons all-the-icons memoize font-lock+ helm-pydoc helm-css-scss helm-company helm-c-yasnippet yapfify pyvenv pytest pyenv-mode py-isort pip-requirements live-py-mode hy-mode cython-mode company-anaconda anaconda-mode pythonic groovy-mode helm-themes helm-swoop helm-projectile helm-mode-manager helm-gitignore helm-flx helm-descbinds helm-ag ace-jump-helm-line ranger mmm-mode markdown-toc markdown-mode gh-md rvm ruby-tools ruby-test-mode rubocop rspec-mode robe rbenv rake minitest chruby bundler inf-ruby org-projectile org-category-capture org-present org-pomodoro alert log4e gntp org-download htmlize gnuplot company-web web-completion-data xterm-color smeargle shell-pop orgit multi-term magit-gitflow gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link evil-magit magit magit-popup eshell-z eshell-prompt-extras esh-help web-mode tagedit slim-mode scss-mode sass-mode pug-mode less-css-mode haml-mode emmet-mode git-gutter-fringe+ git-gutter-fringe fringe-helper git-gutter+ git-commit with-editor git-gutter diff-hl evil-commentary zenburn-theme zen-and-art-theme underwater-theme ujelly-theme twilight-theme twilight-bright-theme twilight-anti-bright-theme toxi-theme tao-theme tangotango-theme tango-plus-theme tango-2-theme sunny-day-theme sublime-themes subatomic256-theme subatomic-theme spacegray-theme soothe-theme solarized-theme soft-stone-theme soft-morning-theme soft-charcoal-theme smyx-theme seti-theme reverse-theme railscasts-theme purple-haze-theme professional-theme planet-theme phoenix-dark-pink-theme phoenix-dark-mono-theme organic-green-theme omtose-phellack-theme oldlace-theme occidental-theme obsidian-theme noctilux-theme naquadah-theme mustang-theme monokai-theme monochrome-theme molokai-theme moe-theme minimal-theme material-theme majapahit-theme madhat2r-theme lush-theme light-soap-theme jbeans-theme jazz-theme ir-black-theme inkpot-theme heroku-theme hemisu-theme hc-zenburn-theme gruvbox-theme gruber-darker-theme grandshell-theme gotham-theme gandalf-theme flatui-theme flatland-theme farmhouse-theme espresso-theme dracula-theme django-theme darktooth-theme autothemer darkokai-theme darkmine-theme darkburn-theme dakrone-theme cyberpunk-theme color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized clues-theme cherry-blossom-theme busybee-theme bubbleberry-theme birds-of-paradise-plus-theme badwolf-theme apropospriate-theme anti-zenburn-theme ample-zen-theme ample-theme alect-themes afternoon-theme reveal-in-osx-finder pbcopy osx-trash osx-dictionary launchctl flyspell-correct-ivy flyspell-correct flycheck-pos-tip pos-tip flycheck company-tern dash-functional tern auto-dictionary web-beautify livid-mode skewer-mode simple-httpd json-mode json-snatcher json-reformat js2-refactor multiple-cursors js2-mode js-doc coffee-mode fuzzy company-statistics company auto-yasnippet yasnippet ac-ispell auto-complete yaml-mode ws-butler winum which-key wgrep volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline smex restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint ivy-hydra info+ indent-guide hydra hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation hide-comnt help-fns+ helm-make helm helm-core google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu highlight elisp-slime-nav dumb-jump popup f dash s diminish define-word counsel-projectile projectile pkg-info epl counsel swiper ivy column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed async aggressive-indent adaptive-wrap ace-window ace-link avy)))
+ '(safe-local-variable-values
+   (quote
+    ((eval progn
+           (add-to-list
+            (quote exec-path)
+            (concat
+             (locate-dominating-file default-directory ".dir-locals.el")
+             "node_modules/.bin/")))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+)
